7c5caa1260b994d5a21088701080854a2a5819a2
kennysmithderulo-cmyk <kennysmithderulo@gmail.com>
2026-09-21 18:13:46 +0000

    Add InstallBanner and MobileNav to PageWrapper

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import { SiteHeader } from "./SiteHeader";
 import { SiteFooter } from "./SiteFooter";
+import { InstallBanner } from "./InstallBanner";
+import { MobileNav } from "./MobileNav";
 
 export function PageWrapper({ children }: { children: ReactNode }) {
   return (
@@ -8,6 +10,8 @@ export function PageWrapper({ children }: { children: ReactNode }) {
       <SiteHeader />
       {children}
       <SiteFooter />
+      <InstallBanner />
+      <MobileNav />
     </div>
   );
 }
