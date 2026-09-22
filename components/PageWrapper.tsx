36dcd93c01bbb928be6d56d4dcd305d3c8b74191
kennysmithderulo-cmyk <kennysmithderulo@gmail.com>
2026-09-22 07:31:16 +0000

    Change background color of PageWrapper component

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -6,7 +6,7 @@ import { MobileNav } from "./MobileNav";
 
 export function PageWrapper({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-[#070f26] text-white">
+    <div className="min-h-screen bg-white text-[#070f26]">
       <SiteHeader />
       {children}
       <SiteFooter />
